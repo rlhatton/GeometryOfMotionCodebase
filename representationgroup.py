adb594963209eb29d9864b5add2bcd418858189d
Ross Hatton <rlhatton@Mieli.local>
2024-05-27 12:20:24 -0700

    representation group composition examples working

--- representationgroup.py
+++ representationgroup.py
@@ -9,31 +9,39 @@ class RepresentationGroup(gp.Group):
     def __init__(self,
                  representation_function_list,
                  identity,
-                 specification_chart = 0,
+                 specification_chart=0,
                  transition_table=((None,))
                  ):
 
         # Initialize the representation group as a group, using None for the attributes we are going to re-implement
-        super().__init__(None,              # Operation list
-                         None,              # Identity list
-                         None,              # Inverse function list
+        super().__init__(None,  # Operation list
+                         None,  # Identity list
+                         None,  # Inverse function list
                          transition_table)
+
+        # Save the representation function as an instance attribute, wrapping it in a tuple if provided as a raw
+        # function
+        self.representation_function_list = ut.ensureTuple(representation_function_list)
 
         # Store the identity input as the group identity representation, passing it through the appropriate
         # representation function if necessary
-        if identity.ndim == 2:
-            self.identity_representation = identity
+
+        #identity = np.array(identity, dtype=float)
+        if (np.squeeze(identity)).ndim == 2:
+            identity_representation = identity
         else:
-            self.identity_representation = representation_function_list[specification_chart](identity)
+            identity_representation = self.representation_function_list[specification_chart](identity)
+
+        self.identity_rep = identity_representation
 
     def element(self,
-                value,
+                representation,
                 initial_chart=0):
 
         """Instantiate a group element with a specified value"""
         g = RepresentationGroupElement(self,
-                         value,
-                         initial_chart)
+                                       representation,
+                                       initial_chart)
         return g
 
     def identity_element(self,
@@ -41,8 +49,8 @@ class RepresentationGroup(gp.Group):
 
         """Instantiate a group element at the identity"""
         g = RepresentationGroupElement(self,
-                         'identity',
-                         initial_chart)
+                                       'identity',
+                                       initial_chart)
 
         return g
 
@@ -53,30 +61,36 @@ class RepresentationGroupElement(gp.GroupElement):
                  group,
                  representation,
                  initial_chart=0):
-
         # Handle the identity-element value keyword
         if isinstance(representation, str) and (representation == 'identity'):
             representation = group.identity_representation
 
         # Use the provided inputs to generate the group-element properties of the group element
         super().__init__(group,
-                         None,           # Don't pass in an initial value; we are making value a property that depends on the representation and chart
+                         None, # Don't pass in an initial value; we are making value a property that depends on the
+                               # representation and chart
                          initial_chart)
 
-        # Store the representation
+        # Store the representation, passing it through the representation function if necessary
+        #representation = np.array(representation, dtype=float)
+        if (np.squeeze(representation)).ndim == 2:
+            pass
+        else:
+            representation = group.representation_function_list[initial_chart](representation)
+
         self.rep = representation
 
     def left_action(self,
                     g_right):
-
         g_composed_rep = np.matmul(self.rep, g_right.rep)
 
-        return g_composed_rep
-
+        return RepresentationGroupElement(self.group,
+                                          g_composed_rep,
+                                          self.current_chart)
     def right_action(self,
-                    g_left):
+                     g_left):
         g_composed_rep = np.matmul(g_left.rep, self.rep)
 
-        return g_composed_rep
-
-
+        return RepresentationGroupElement(self.group,
+                                          g_composed_rep,
+                                          self.current_chart)
---
+++ representationgroup_composition_examples.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python3
+import numpy as np
+import representationgroup as rgp
+
+""" Scalar addition """
+
+def scalar_addition_rep(g_value):
+
+    g_rep = [[1, g_value], [0, 1]]
+    return g_rep
+
+
+R1plus = rgp.RepresentationGroup(scalar_addition_rep, 0)
+
+g1 = R1plus.element(2)
+g2 = R1plus.element(3)
+
+g3 = g1 * g2
+
+print("Addition-group composition of " +str(g1.rep) + " and " +str(g2.rep) + " is " + str(g3.rep))
+
+""" Scalar addition """
+
+def scalar_multiplication_rep(g_value):
+
+    g_rep = [[g_value]]
+    return g_rep
+
+
+R1times = rgp.RepresentationGroup(scalar_multiplication_rep, 1)
+
+g1 = R1times.element(2.)
+g2 = R1times.element(3.)
+
+g3 = g1 * g2
+
+print("Multiplication-group composition of " +str(g1.rep) + " and " +str(g2.rep) + " is " + str(g3.rep))
+
+def modular_addition_rep(g_value):
+
+    g_radians = 2*np.pi*g_value
+    g_rep = [[np.cos(g_radians), -np.sin(g_radians)],
+             [np.sin(g_radians),  np.cos(g_radians)]]
+    return g_rep
+
+
+S1plus = rgp.RepresentationGroup(modular_addition_rep, 0)
+
+g1 = S1plus.element(.25)
+g2 = S1plus.element(.875)
+
+g3 = g1 * g2
+
+print("Multiplication-group composition of " +str(g1.rep) + " and " +str(g2.rep) + " is " + str(g3.rep))
