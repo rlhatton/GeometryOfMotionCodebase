e30f12c1eddd2fee6834d8e35d6bf96b0c4eecca
Ross Hatton <rlhatton@10-248-24-179.wireless.oregonstate.edu>
2024-06-11 17:01:35 -0700

    trying to add matplotlib output to examples, but pycharm isn't cooperating

--- Chapter_1_Examples/E105_Cartesian_Polar_Transition.py
+++ Chapter_1_Examples/E105_Cartesian_Polar_Transition.py
@@ -1,17 +1,23 @@
-# Make things print nicely
 import numpy as np
-np.set_printoptions(precision=2)
+import matplotlib.pyplot as plt
+from E100_Construct_R2 import R2  # Get the R2 manifold constructed previously
+import matplotlib
+matplotlib.use('MACOSX')
 
-# Get the R2 manifold as constructed previously
-from E100_Construct_R2 import R2
+# Make things print nicely
+np.set_printoptions(precision=2)
 
 # Make the working manifold for this problem R2
 Q = R2
 
 # Construct a point on the manifold
 q = Q.element([3, 3], 0)
-
 print("Initial configuration in Cartesian coordinates is " + str(q.value))
+fig, ax = plt.subplots()
+ax.plot(q.value, q.value)
+ax.grid(True)
+plt.show()
+
 
 q_polar = q.transition(1)
 
