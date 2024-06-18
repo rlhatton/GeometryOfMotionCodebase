83c1b375686656e9b4daddee23ac3091a05d33c1
Ross Hatton <rlhatton@Mieli.local>
2024-06-17 22:16:27 -0700

    SE2 left and right action illustration done

--- Chapter_1_Examples/E305_SE2_drawing.py
+++ Chapter_1_Examples/E305_SE2_drawing.py
@@ -7,15 +7,29 @@ import numpy as np
 
 spot_color = gplt.crimson
 
+
+# Initial position of elements
 e = SE2.identity_element()
-g = SE2.element([2, 1, np.pi/2])
-h = SE2.element([0, -1, -np.pi/4])
+g = SE2.element([2, 1, np.pi / 2])
+h = SE2.element([0, -1, -np.pi / 4])
 
 a = RigidBody(cornered_triangle(.25, spot_color), e)
 b = RigidBody(cornered_triangle(.25, spot_color), g)
 c = RigidBody(cornered_triangle(.25, spot_color), h)
 
-ax = plt.subplot(1, 1, 1)
+# Elements for second plot
+gh = g*h
+
+d = RigidBody(cornered_triangle(.25, spot_color), gh)
+
+# Elements for third plot
+g_theta = SE2.element([0, 0, g[2]])
+g_theta_h = g_theta * h
+
+f = RigidBody(cornered_triangle(.25, spot_color), g_theta_h)
+
+# Plotting commands
+ax = plt.subplot(1, 3, 1)
 a.draw(ax)
 b.draw(ax)
 c.draw(ax)
@@ -25,5 +39,28 @@ ax.set_aspect('equal')
 ax.axhline(0, color='black', zorder=-1)
 ax.axvline(0, color='black', zorder=-1)
 
+ax = plt.subplot(1, 3, 2)
+b.draw(ax)
+d.draw(ax)
+ax.set_xlim(-1, 4)
+ax.set_ylim(-2, 3)
+ax.arrow(g[0], g[1], -.75, 0, length_includes_head=True,  width=.01, head_width=0.2, overhang=.5)
+ax.arrow(g[0], g[1], 0, .75, length_includes_head=True,  width=.01, head_width=0.2, overhang=.5)
+ax.set_aspect('equal')
+ax.axhline(0, color='black', zorder=-1)
+ax.axvline(0, color='black', zorder=-1)
+
+ax = plt.subplot(1, 3, 3)
+c.draw(ax)
+f.draw(ax)
+d.draw(ax)
+ax.set_xlim(-1, 4)
+ax.set_ylim(-2, 3)
+ax.set_aspect('equal')
+circle = plt.Circle([0, 0], 1, edgecolor='black', facecolor='none', linestyle='dashed')
+ax.add_artist(circle)
+ax.arrow(g_theta_h[0], g_theta_h[1], .9*(gh[0]-g_theta_h[0]), .9*(gh[1]-g_theta_h[1]), zorder=0, length_includes_head=True,  width=.01, head_width=0.2, overhang=.5)
+ax.axhline(0, color='black', zorder=-1)
+ax.axvline(0, color='black', zorder=-1)
 
 plt.show()
